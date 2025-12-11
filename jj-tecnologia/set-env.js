443d21c427ac6a98d5140fbe118f2c3ff1087bcf
Joselyn96 <joselynvalverde6916@gmail.com>
2025-12-11 16:11:59 -0500

    Se ha implementado set-env.js

---
+++ jj-tecnologia/set-env.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const targetPath = './src/environments/environment.ts';
+
+const envFileContent = `
+export const environment = {
+  production: false,
+  supabaseUrl: '${process.env.SUPABASE_URL}',
+  supabaseKey: '${process.env.SUPABASE_KEY}',
+};
+`;
+
+fs.writeFileSync(targetPath, envFileContent);
+console.log('Environment file generated.');
