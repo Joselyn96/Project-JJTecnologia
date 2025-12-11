641718c6e208df663108a8ff228b7a3468edcf66
Joselyn96 <joselynvalverde6916@gmail.com>
2025-12-11 17:31:50 -0500

    Se ha cambiado set-env.js con logs

--- jj-tecnologia/set-env.js
+++ jj-tecnologia/set-env.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 
 const targetPath = './src/environments/environment.ts';
-
+console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
+console.log('SUPABASE_KEY:', process.env.SUPABASE_KEY ? 'SET' : 'NOT SET');
 const envFileContent = `
 export const environment = {
   production: true,
