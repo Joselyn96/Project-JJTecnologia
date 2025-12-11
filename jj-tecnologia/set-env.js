026a8826c5721fce5de3e6992a0f79bd955f088c
Joselyn96 <joselynvalverde6916@gmail.com>
2025-12-11 17:41:08 -0500

    Se ha cambiado se-event.js hardcodeado

--- jj-tecnologia/set-env.js
+++ jj-tecnologia/set-env.js
@@ -1,13 +1,19 @@
 const fs = require('fs');
 
 const targetPath = './src/environments/environment.ts';
-console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
-console.log('SUPABASE_KEY:', process.env.SUPABASE_KEY ? 'SET' : 'NOT SET');
+
+// Coloca tus credenciales REALES aquí
+const supabaseUrl = process.env.SUPABASE_URL || 'https://zlblrmelaalhdguylxty.supabase.co';
+const supabaseKey = process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsYmxybWVsYWFsaGRndXlseHR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk0MjkyMDUsImV4cCI6MjA3NTAwNTIwNX0.G7lLxMow9eXTLbYQbS8EV-UK8HgxmGOTbtLL1sEK0LI';
+
+console.log('SUPABASE_URL:', supabaseUrl);
+console.log('SUPABASE_KEY:', supabaseKey ? 'SET' : 'NOT SET');
+
 const envFileContent = `
 export const environment = {
   production: true,
-  supabaseUrl: '${process.env.SUPABASE_URL}',
-  supabaseKey: '${process.env.SUPABASE_KEY}',
+  supabaseUrl: '${supabaseUrl}',
+  supabaseKey: '${supabaseKey}',
 };
 `;
 
